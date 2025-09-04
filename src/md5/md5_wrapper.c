09e77e80bb9df9968b0a5d5e95ec71f991d8902f
Elio Bonutto <elio.bonutto@gmail.com>
2025-09-04 18:05:55 +0200

    feat: add wrapper

--- src/md5/md5_wrapper.c
+++ src/md5/md5_wrapper.c
@@ -3,10 +3,11 @@
 
 // void md5( const uint8_t *data, size_t len, uint8_t hash[16] )
 
-// void md5_wrapper( t_opts *opts )
-// {
-// 	uint8_t hash[16];
+void md5_wrapper( t_opts *opts )
+{
+	(void)opts;
+	// uint8_t hash[16];
 
-// 	const uint8_t data = 
-// 	md5();
-// }
+	// const uint8_t data = 
+	// md5();
+}
--- src/sha256/sha256_wrapper.c
+++ src/sha256/sha256_wrapper.c
@@ -3,9 +3,20 @@
 
 void sha256_wrapper( t_opts *opts )
 {
-	char *line;
-	size_t  len;
-	get_next_line( &line, &len );
-	sha256();
+	// char *line;
+	// int  len;
+	// uint8_t hash[32];
+
+	char buf[1000];
+	int r = read( 0, buf, 1000 );
+	buf[r] = '\0';
+	printf("%s", buf);
+
+	// get_next_line( &line, &len, 0 );
+	// printf("%s", line);
+	// sha256((uint8_t *)line, ft_strlen(line), hash);
+	// for (int i = 0; i < 32; i++)
+	// 	printf("%02x", hash[i]);
+	// free(line);
 	(void)opts;
 }
