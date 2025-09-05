aa8e9bb154815b8a9d222a7b18fa066f02a31456
Elio Bonutto <elio.bonutto@gmail.com>
2025-09-05 18:46:46 +0200

    feat: architecture

--- src/clear.c
+++
@@ -1,7 +0,0 @@
-#include "ft_ssl.h"
-
-void ssl_ctx_clear( t_ssl_ctx *ssl_ctx )
-{
-	ft_lstfree( &ssl_ctx->strings );
-	ft_lstfree( &ssl_ctx->files );
-}
--- src/commands.c
+++
@@ -1,8 +0,0 @@
-#include "ft_ssl.h"
-
-t_cmd g_cmds[] = {
-	{"md5", md5_wrapper},
-	{"sha256", sha256_wrapper},
-	{"whirlpool", whirlpool_wrapper},
-	{NULL, NULL},
-};
