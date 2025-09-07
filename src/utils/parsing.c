808cda5042d3a2ecd6144c96505c57680d5b9ff2
X03phy <elio.bonutto@gmail.com>
2025-09-08 00:06:28 +0200

    add ft_strcasecmp() instead of ft_strcmp() and passing argv[1] as an argument

--- src/utils/parsing.c
+++ src/utils/parsing.c
@@ -1,11 +1,16 @@
 #include "ft_ssl.h"
 
-t_ssl_wfunc parse_cmd( t_ssl_cmd *cmd )
+/* For ft_strcasecmp() */
+#include "string.h"
+
+
+t_ssl_wfunc parse_cmd( t_ssl_cmd *cmd, char *arg )
 {
 	for ( int i = 0; g_cmds[i].name; ++i )
 	{
-		if ( ft_strcmp( g_cmds[i].name, cmd->name ) == 0 )
+		if ( ft_strcasecmp( g_cmds[i].name, arg ) == 0 )
 		{
+			cmd->name = g_cmds[i].name;
 			cmd->type = CMD_HASH;
 			cmd->hash.hash_size =  g_cmds[i].hash.hash_size;
 			cmd->hash.hash_func =  g_cmds[i].hash.hash_func;
