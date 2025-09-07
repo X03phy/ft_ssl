3cf077bf797fcfecc8ef9bde532b66c3b8b70a9e
X03phy <elio.bonutto@gmail.com>
2025-09-08 00:00:42 +0200

    fix: norm and removing one include

--- include/whirlpool.h
+++ include/whirlpool.h
@@ -7,9 +7,9 @@
 /* For size_t */
 #include <stddef.h>
 
-/* For memset() */
-#include "memory.h"
 
+/* Prototypes */
 void whirlpool( const uint8_t *data, size_t len, uint8_t hash[64] );
+
 
 #endif
