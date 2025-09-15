67c2ebfdf7ebf1559703a0bd0fcc9d763d55a0f6
Elio Bonutto <elio.bonutto@gmail.com>
2025-09-15 11:20:59 +0200

    feat: base64

--- src/encoding/encoding_wrapper.c
+++ src/encoding/encoding_wrapper.c
@@ -1,22 +1,123 @@
 #include "ft_ssl.h"
+
+/* For open() */
+#include <fcntl.h>
+
+/* For lists */
+#include "linked_list.h"
+
+/* For ft_putstr_fd(), ft_putchar_fd() */
+#include "fd.h"
+
+/* For ft_memset() */
+#include "memory.h"
+
+/* For ft_strcmp(), ft_strlen() */
+#include "string.h"
+
+
+/* Macros */
+#define FLAG_E 0
+#define FLAG_D 1
+
+
+/* Structures */
+typedef struct s_ssl_encoding_ctx {
+	int    flags;
+	char   *input;
+	size_t len;
+	t_list *inf;
+	t_list *out;
+} t_ssl_encoding_ctx;
+
+
+/* Code */
+static int parse_input( int argc, char **argv, t_ssl_encoding_ctx *ctx )
+{
+	int    i = 2; // argv[1] = command
+	t_list *tmp;
+
+	while ( i < argc )
+	{
+		if ( argv[i][0] == '-' )
+		{
+			if ( ft_strcmp( argv[i], "-e" ) == 0 )
+				ctx->flags |= 1 << FLAG_E;
+			else if ( ft_strcmp( argv[i], "-d" ) == 0 )
+				ctx->flags |= 1 << FLAG_D;
+			else if ( ft_strcmp( argv[i], "-i" ) == 0 )
+			{
+				if ( i + 1 >= argc )
+				{
+					ft_putstr_fd( "ft_ssl: option requires an argument -- s\n", 2 );
+					print_help();
+					return ( 0 );
+				}
+
+				tmp = ft_lstnew( argv[++i] );
+				if ( !tmp )
+				{
+					print_internal_error( "ft_lstnew()" );
+					return ( 0 );
+				}
+				ft_lstadd_back( &ctx->inf, tmp );	
+			}
+			else if ( ft_strcmp( argv[i], "-o" ) == 0 )
+			{
+				if ( i + 1 >= argc )
+				{
+					ft_putstr_fd( "ft_ssl: option requires an argument\n", 2 );
+					print_help();
+					return ( 0 );
+				}
+
+				tmp = ft_lstnew( argv[++i] );
+				if ( !tmp )
+				{
+					print_internal_error( "ft_lstnew()" );
+					return ( 0 );
+				}
+				ft_lstadd_back( &ctx->out, tmp );
+			}
+			else
+			{
+				print_invalid_flag( argv[i] );
+				print_help();
+				return ( 0 );
+			}
+		}
+		++i;
+	}
+
+	if ( !ctx->inf )
+	{
+		if ( !get_content_fd( 0, &(ctx->input), &(ctx->len) ) )
+			return ( 0 );
+	}
+
+	return ( 1 );
+}
+
 #include <stdio.h>
-#include <stdlib.h>
 int encoding_wrapper( int argc, char **argv, t_ssl_cmd *cmd )
 {
-	const char *line = "yeah";
-	size_t n = 4;
-	uint8_t *yeah = NULL;
-	size_t out_len;
+	// int exit_code;
+	t_ssl_encoding_ctx ctx;
+	size_t len;
+	uint8_t *text;
 
-	(void)argc;
-	(void)argv;
+	ft_memset( &ctx, 0, sizeof(ctx) );
+
+	if ( !parse_input( argc, argv, &ctx ) )
+		return ( 1 );
 
 
-	out_len = 4 * ((n + 2) / 3) + 1;
-    yeah = malloc(out_len);
+	len = 4 * ((ctx.len + 2) / 3) + 1;
+	text = malloc(sizeof(char)* len);
 
-	cmd->encoding.encode_func( ( const uint8_t *)line, n, yeah );
+	printf( "%ld\n", len );
+	cmd->encoding.encode_func( ( const uint8_t *)ctx.input, ctx.len, text );
 
-	printf( "%s\n", yeah );
+	printf( "%s\n", text );
 	return ( 1 );
 }
