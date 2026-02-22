ca2b13a7afda77e7b7151b43873ff24b5f4affe9
Elio Bonutto <elio@pc-183.home>
2026-02-22 23:04:02 +0100

    feat: add free functions

--- src/hash/hash_main.c
+++ src/hash/hash_main.c
@@ -1,6 +1,7 @@
 #include "hash/hash.h"
+#include "utils/colors.h"
 #include <string.h> // strcmp(), memset()
-#include <stdio.h>  // NULL, printf()
+#include <stdio.h>  // NULL, fprintf, printf()
 #include <stdlib.h> // malloc()
 
 
@@ -22,52 +23,36 @@ static const t_hash_algo *get_hash_algo(const char *name)
 }
 
 
-void free_hash_ctx(t_hash_ctx *hctx)
-{
-	t_list *node, *next;
-
-	node = hctx->inputs;
-	while (node) {
-		next = node->next;
-		free(node->data);
-		free(node);
-		node = next;
-	}
-
-	if (hctx->algo_ctx)
-		free(hctx->algo_ctx);
-}
-
-
 int hash_main(int argc, char **argv)
 {
-	int        ret;
 	t_hash_ctx hctx;
+	int        ret;
 
 	memset(&hctx, 0, sizeof(hctx));
 
 	hctx.algo = get_hash_algo(argv[1]);
-	if (hctx.algo == NULL)
-		return (0);
 
-	ret = parse_inputs(&hctx, argc, argv);
-	if (!ret) {
-		free_hash_ctx(&hctx);
+	if (parse_inputs(&hctx, argc, argv)) {
+		list_clear(&hctx.inputs, free);
 		return (0);
 	}
 
-	if (hctx.flags & (1 << FLAG_H))
+	if (hctx.flags & (1 << FLAG_H)) {
 		printf(HASH_HELP_FORMAT, argv[0], argv[1]);
+		list_clear(&hctx.inputs, free);
+		return (1);
+	}
 
 	hctx.algo_ctx = malloc(hctx.algo->ctx_size);
 	if (!hctx.algo_ctx) {
 		perror("malloc() failed");
-		free_hash_ctx(&hctx);
+		list_clear(&hctx.inputs, free);
 		return (0);
 	}
 
 	ret = process_inputs(&hctx);
 
-	free_hash_ctx(&hctx);
+	list_clear(&hctx.inputs, free);
+	free(hctx.algo_ctx);
 	return (ret);
 }
