60a8648026828083553435b4555fa54f8757dea1
Elio Bonutto <elio@pc-183.home>
2026-02-22 23:02:15 +0100

    feat: add functions

--- include/utils/list.h
+++ include/utils/list.h
@@ -1,8 +1,9 @@
 #pragma once
 
+
 /*
  * Structures
-*/
+ */
 
 typedef struct s_list {
 	void          *data;
@@ -11,9 +12,11 @@ typedef struct s_list {
 
 
 /*
- * prototypes
-*/
+ * Prototypes
+ */
 
+void   list_clear(t_list **list, void (*del)(void *));
+void   list_free(t_list **list);
 t_list *list_last(t_list *list);
-void list_push_back(t_list **list, t_list *new);
 t_list *list_new(void *data);
+void   list_push_back(t_list **list, t_list *new);
--- include/utils/str.h
+++ include/utils/str.h
@@ -1,3 +1,9 @@
 #pragma once
 
+
+/*
+ * Prototypes
+ */
+
+char **ft_split(const char *s, char c);
 char *strjoin(const char *s1, const char *s2);
