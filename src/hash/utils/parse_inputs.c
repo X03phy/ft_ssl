983d52ac8c1c86734816e6970874cca7036f493f
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-02-25 17:21:25 +0100

    fix: norm -> multiple functions for parse_flags

--- src/hash/utils/parse_inputs.c
+++ src/hash/utils/parse_inputs.c
@@ -54,37 +54,57 @@ static int add_input(t_hash_ctx *ctx, const e_hash_input_type type, char *data)
 }
 
 
+static void print_error(const char *format, char **argv, int i)
+{
+	fprintf(stderr, format,
+	        argv[0], argv[1], argv[i]);
+	fprintf(stderr, "\n");
+	fprintf(stderr, HASH_HELP_FORMAT, argv[0], argv[1]);
+}
+
+
+static int parse_flag(t_hash_ctx *ctx, int *i, int argc, char **argv)
+{
+	if (strcmp("-h", argv[*i]) == 0 ||
+	    strcmp("--help", argv[*i]) == 0) {
+		ctx->flags |= 1 << FLAG_H;
+		return (1);
+	} else if ((strcmp("-p", argv[*i]) == 0) ||
+	           (strcmp("--append", argv[*i]) == 0)) {
+		ctx->flags |= 1 << FLAG_P;
+	} else if ((strcmp("-q", argv[*i]) == 0) ||
+	           (strcmp("--quiet", argv[*i]) == 0)) {
+		ctx->flags |= 1 << FLAG_Q;
+	} else if ((strcmp("-r", argv[*i]) == 0) ||
+	           (strcmp("--reverse", argv[*i]) == 0)) {
+		ctx->flags |= 1 << FLAG_R;
+	} else if ((strcmp("-s", argv[*i]) == 0) ||
+	           (strcmp("--string", argv[*i]) == 0)) {
+		if (*i + 1 >= argc) {
+			print_error(HASH_EXPECTING_ARGUMENT_AFTER_FLAG_FORMAT,
+			            argv, *i);
+			return (0);
+		}
+		*i += 1;
+		if (!add_input(ctx, HASH_INPUT_STRING, argv[*i]))
+			return (0);
+	} else {
+		print_error(HASH_INVALID_OPTION_FORMAT, argv, *i);
+		return (0);
+	}
+
+	return (1);
+}
+
+
 int parse_inputs(t_hash_ctx *ctx, int argc, char **argv)
 {
 	int i = 2;
 
 	while (i < argc) {
 		if (argv[i][0] == '-') {
-			if ((argv[i][1] == 'h' && argv[i][2] == '\0') || strcmp("--help", argv[i]) == 0) {
-				ctx->flags |= 1 << FLAG_H;
-				return (1);
-			} else if ((argv[i][1] == 'p' && argv[i][2] == '\0') || strcmp("--append", argv[i]) == 0) {
-				ctx->flags |= 1 << FLAG_P;
-			} else if ((argv[i][1] == 'q' && argv[i][2] == '\0') || strcmp("--quiet", argv[i]) == 0) {
-				ctx->flags |= 1 << FLAG_Q;
-			} else if ((argv[i][1] == 'r' && argv[i][2] == '\0') || strcmp("--reverse", argv[i]) == 0) {
-				ctx->flags |= 1 << FLAG_R;
-			} else if ((argv[i][1] == 's' && argv[i][2] == '\0') || strcmp("--string", argv[i]) == 0) {
-				if (i + 1 >= argc) {
-					fprintf(stderr, HASH_EXPECTING_ARGUMENT_AFTER_FLAG_FORMAT, argv[0], argv[1], argv[i]);
-					fprintf(stderr, "\n");
-					fprintf(stderr, HASH_HELP_FORMAT, argv[0], argv[1]);
-					return (0);
-				}
-				i += 1;
-				if (!add_input(ctx, HASH_INPUT_STRING, argv[i]))
-					return (0);
-			} else {
-				fprintf(stderr, HASH_INVALID_OPTION_FORMAT, argv[0], argv[1], argv[i]);
-				fprintf(stderr, "\n");
-				fprintf(stderr, HASH_HELP_FORMAT, argv[0], argv[1]);
+			if (!parse_flag(ctx, &i, argc, argv))
 				return (0);
-			}
 		} else {
 			if (!add_input(ctx, HASH_INPUT_FILE, argv[i]))
 				return (0);
